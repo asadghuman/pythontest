fb77847d957b369a614ab152283443cbeed85a83
asadghuman <asadghumman92.ag@gmail.com>
2025-04-21 20:45:54 +0100

    first commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, this is a simple Python app!"
+    return "Hello, this is a simple Python app!asad"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6000)
